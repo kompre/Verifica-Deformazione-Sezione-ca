18b509e3cf1468ad2432f48f8155b7a00f393d93
kompre <s.follador@gmail.com>
2017-01-02 10:59:02 +0100

    riorganizzazione file

--- intIndef.m
+++ intIndef.m
@@ -1,5 +1,5 @@
 function [ f1 ] = intIndef( f0, x )
-%INTINDEF Summary of this function goes here
+%INTINDEF work in progress
 %   Detailed explanation goes here
 
 f1 = @(f0,x,dx,varargin) sum(f0( 0:dx:x, varargin)*dx);
