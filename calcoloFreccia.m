e930928b50d666515dfa23c88ff713c0653498c5
kompre <s.follador@gmail.com>
2017-01-03 19:15:14 +0100

    add funzione calcoloFreccia

---
+++ calcoloFreccia.m
@@ -0,0 +1,74 @@
+function [s_min] = calcoloFreccia( L, geom, arm, mat, soll, cost, dx)
+%% dati sezione
+% dati geometrici
+% b = [1000];
+% L = 4;  % [m] luce libera di inflessione
+% dati armatura
+% nb = [5;5];
+% diam = [12;12];
+% d = [40;260];
+% dati materiale
+% mat.cls = derivaCaratteristicheCA(25,30);
+% mat.steel = derivaCaratteristicheAcciaio;
+n = mat.steel.Es/mat.cls.E_cm; % coefficiente di omogeneizzazione
+% dati sollecitazione
+Fd = soll.q;  % [kN/m2] carico agente in condizioni SLE
+Ned = soll.N;
+M = soll.M;   % funzione del momento sollecitante per trave 1 campata
+% cost.c = 0.5;    % parametro per il calcolo di zeta [0.5 per carichi lunga durata/ripetuti, 1 per breve duarat]
+% % condizioni al contorno
+% cost.rot = 0;   % c.i. rotazione
+% cost.spo = 0;   % c.i. spostamento
+% dati punti di calcolo
+% dx = .001;  % passo dell'integrazione
+x = 0:dx:L; % vettore delle ascisse dei punti di calcolo
+%%
+sez = rettangolo(geom.b, geom.h, geom.x0, geom.y0, 100, 100);    % discretizzazione della sezione in c.a. in rettangoli infitesimali
+reb = rebarDistr(arm.nb, arm.diam, arm.d, -geom.b/2, geom.b/2);   % distribuzione delle barre di armatura nella sezione
+
+% caratteristiche sezione cls
+prop.cls = sectionProperty(sez);    
+
+% caratteristiche sezione acciaio
+prop.steel = rebarProperty(reb);
+
+% caratteristiche sezione ca (cls+acciaio)
+prop.ca.A = prop.cls.A + n*prop.steel.A;    % area omogeneizzata rispetto al cls
+prop.ca.Sx = prop.cls.Sx + n*prop.steel.Sx; % momento statico omogeneizzato rispetto al cls
+prop.ca.yg = prop.ca.Sx/prop.ca.A;  % baricentro omogeneizzato sezione c.a.
+prop.ca.Jxg = prop.cls.Jxg + prop.cls.A*(prop.cls.yg-prop.ca.yg)^2 + n*(prop.steel.Jxg + prop.steel.A*(prop.steel.yg-prop.ca.yg)^2);
+
+%% Calcolo del momento di prima fessurazione
+W_min = prop.ca.Jxg/min(prop.ca.yg,(sum(geom.h)-prop.ca.yg));
+Mf = W_min*mat.cls.f_ctm*1e-6; % momento di prima fessurazione [kNm]
+alpha.nf = (mat.cls.E_cm * prop.cls.Jxg) / (mat.steel.Es/n *prop.ca.Jxg); % rapporto momento di inerzia sezione non fessurata
+%% Calcolo asse neutro per sezione fessurata
+[sezfes.x, sezfes.A, sezfes.Sx, sezfes.Jx] = asseNeutro(geom.b, geom.h, reb(:,3),reb(:,2),Ned,Mf,n); % N e M sono necessari sono nel caso di N > 0
+
+%% Calcolo delle deformazioni
+% La deformata è stimata moltiplicando la freccia elastica per un fattore
+% amplificativo pari al rapporto tra il momento di inerzia della sezione
+% lorda di cls (è ignorato il contributo delle barre) e il momento di
+% inerzia intermedio tra la sezione non fessurata e la sezione fessurata
+% (con barre). 
+
+Med = M(Fd,L,x);    %calcolo del momento sollecitante lungo l'asse
+
+momenti_notevoli = [Med(1), max(Med), Med(end)];    % momenti alle sezioni notevoli
+j = 0;
+for i = 1:length(momenti_notevoli)
+    beta = Mf/momenti_notevoli(i);
+    if beta < 1
+        j = j + 1;
+        zeta(j) = 1 - cost.c*beta^2;
+    end
+end
+Jef = zeta * sezfes.Jx + (1 - zeta) * prop.cls.Jxg; % momento di inerzia efficace
+Jef = mean(Jef);
+
+curvatura = Med*1e6/(mat.cls.E_cm * Jef);  % curvatura
+rotazione = integraleIndefinito(curvatura, x*1e3, cost.rot);  % rotazione
+spostamento = integraleIndefinito(rotazione, x*1e3, cost.spo);  % rotazione
+s_min = min(spostamento); % freccia massima
+end
+
--- main.m
+++ main.m
@@ -1,75 +1,93 @@
 clearvars
 clc
-%% dati sezione
-% dati geometrici
-b = [1000];
-h = [300];
-l = 4;  % [m] luce libera di inflessione
-% dati armatura
-nb = [5;5];
-diam = [12;12];
-d = [40;260];
+%% Dimensionamento Platea
+% in condizioni SLE la platea è soggeta al carico q = 66.3 kN/m^2. Il
+% criterio dimensionante è la freccia nel punto più sfavorevole, i.e. nel
+% punto medio della platea compreso tra due pali. Pertanto la luce
+% effettiva di calcolo è pari a (Lpl + Ltr)^2.
+
+% Dati geometrici sezione della platea
+geom.pl.b = 1000;
+geom.pl.h = 300;
+geom.pl.x0 = 0;
+geom.pl.y0 = 0;
+% Dati geometrici sezione della trave
+geom.tr.b = 500;
+geom.tr.h = 800;
+geom.tr.x0 = 0;
+geom.tr.y0 = 0;
+
 % dati materiale
 mat.cls = derivaCaratteristicheCA(25,30);
 mat.steel = derivaCaratteristicheAcciaio;
-n = mat.steel.Es/mat.cls.E_cm; % coefficiente di omogeneizzazione
-% dati sollecitazione
-Fd = 66.3;  % [kN/m2] carico agente in condizioni SLE
-Ned = 0;
-M = @(q,l,x) -q*x.^2/2 + q*l/2*x - q*l^2/12;   % funzione del momento sollecitante per trave appoggio appoggio
-% dati punti di calcolo
-dx = .001;  % passo dell'integrazione
-x = 0:dx:l; % vettore delle ascisse dei punti di calcolo
-%% calcolo inerzia sezione interamente reagente
-sez = rettangolo(b,h,0,0,100,100);    % discretizzazione della sezione in c.a. in rettangoli infitesimali
-reb = rebarDistr(nb, diam, d, -b/2, b/2);   % distribuzione delle barre di armatura nella sezione
 
-% caratteristiche sezione cls
-prop.cls = sectionProperty(sez);    
+% dati sollecitazione della platea
+soll.pl.q = 66.3;  % [kN/m2] carico agente in condizioni SLE
+soll.pl.qSLU = 96.19;  % [kN/m2] carico agente in condizioni SLU
+soll.pl.N = 0;
+soll.pl.M = @(q,l,x) -q*x.^2/2 + q*l/2*x - q*l^2/12;   % funzione del momento sollecitante per trave 1 campata
+% dati sollecitazione della trave
+soll.tr.q = 0;  % inizializzo il valore di carico da stimare per ogni iteraizione in funzione della luce di inflessione della platea
+soll.tr.qSLU = 0;  % idem come sopra
+soll.tr.N = 0;
+soll.tr.M = @(q,l,x) -q*x.^2/2 + q*l/2*x - q*l^2/12;   % funzione del momento sollecitante per trave 1 campata
 
-% caratteristiche sezione acciaio
-prop.steel = rebarProperty(reb);
+% costanti
+cost.c = 0.5;
+cost.rot = 0;
+cost.spo = 0;
+% passo di integrazione
+dx = 0.001;
+% dati armatura platea tipo
+arm.pl.nb = [5;5];
+arm.pl.diam = [12;12];
+arm.pl.d = [40;260];
+% dati armatura trave tipo
+arm.tr.nb = [4;2;2;4];
+arm.tr.diam = [14;14;14;14];
+arm.tr.d = [50;270;530;750];
 
-% caratteristiche sezione ca (cls+acciaio)
-prop.ca.A = prop.cls.A + n*prop.steel.A;    % area omogeneizzata rispetto al cls
-prop.ca.Sx = prop.cls.Sx + n*prop.steel.Sx; % momento statico omogeneizzato rispetto al cls
-prop.ca.yg = prop.ca.Sx/prop.ca.A;  % baricentro omogeneizzato sezione c.a.
-prop.ca.Jxg = prop.cls.Jxg + prop.cls.A*(prop.cls.yg-prop.ca.yg)^2 + n*(prop.steel.Jxg + prop.steel.A*(prop.steel.yg-prop.ca.yg)^2);
+%% VARIAZIONE DELLA LUCE LIBERA DI INFLESSIONE DELLA PLATEA
+L.pl = 1:0.1:6; % [m] variazione della luce libera di inflessione della platea con risoluzione 10 cm
+%% VARIAZIONE DELLA LUCE LIBERA DI INFLESSIONE DELLA TRAVE
+L.tr = 1:0.1:10; % [m] variazione della luce libera di inflessione della platea con risoluzione 10 cm
 
-%% Calcolo del momento di prima fessurazione
-W_min = prop.ca.Jxg/min(prop.ca.yg,(h-prop.ca.yg));
-Mf = W_min*mat.cls.f_ctm*1e-6; % momento di prima fessurazione [kNm]
-alpha.nf = (mat.cls.E_cm * prop.cls.Jxg) / (mat.steel.Es/n *prop.ca.Jxg); % rapporto momento di inerzia sezione non fessurata
-%% Calcolo asse neutro per sezione fessurata
-[sezfes.x, sezfes.A, sezfes.Sx, sezfes.Jx] = asseNeutro(b,h,reb(:,3),reb(:,2),Ned,Mf,n); % N e M sono necessari sono nel caso di N > 0
+arm.pl = repmat(arm.pl, length(L.pl),1);   % prealloco la struttura arm.pl (N)
+arm.tr = repmat(arm.tr, length(L.pl), length(L.tr)); % prealloco la struttura arm.tr  (NxM)
 
-Med = M(Fd,l,x);
-curvatura = zeros(size(Med));
-% for i = 1:length(Med)
-%     if abs(Med(i)) <= Mf
-%         J = prop.ca.Jxg;
-%     else
-%         J = sezfes.Jx;
-%     end
-%     curvatura(i) = Med(i)*1e6/(mat.cls.E_cm*J);
-% end
-curvatura = Med*1e6/(mat.cls.E_cm*prop.ca.Jxg);
+for i_pl = 1:length(L.pl)
+    tic
+    % dimensionamento platea
+    M_max.pl(i_pl) = soll.pl.M(soll.pl.qSLU, L.pl(i_pl), L.pl(i_pl)/2);    % momento in mezzeria, in funzione della luce
+    As_min = M_max.pl(i_pl)*1e6 / (0.9*max(arm.pl(i_pl).d) * mat.steel.fyd / mat.steel.gamma_s) /arm.pl(i_pl).nb(2);   % armatura minima con criterio semplificato dimensionata allo SLU
+    diam_min = ceil( ceil( sqrt( 4*As_min/pi ) ) /2 ) * 2;   % arrotonda ad un diametro reale
+    arm.pl(i_pl).diam = [12;diam_min];
+    % calcolo freccia
+    ris.pl.s_min(i_pl) = calcoloFreccia(L.pl(i_pl), geom.pl, arm.pl(i_pl), mat, soll.pl, cost, dx);
+    
+    % dimensionamento trave
 
-rotazione = zeros(size(curvatura));
-spostamento = zeros(size(curvatura));
+    soll.tr.q = soll.pl.q * L.pl(i_pl);
+    soll.tr.qSLU = soll.pl.qSLU * L.pl(i_pl);   % carico agente sulle travi (carico x area di influenza)
+    for i_tr = 1:length(L.tr)        
+        M_max.tr(i_pl,i_tr) = soll.tr.M(soll.tr.qSLU, L.tr(i_tr), L.tr(i_tr)/2);
+        As_min = M_max.tr(i_pl, i_tr)*1e6 / (0.9*max(arm.tr(i_pl, i_tr).d) * mat.steel.fyd / mat.steel.gamma_s) /arm.tr(i_pl, i_tr).nb(2);   % armatura minima con criterio semplificato dimensionata allo SLU
+        diam_min = ceil( ceil( sqrt( 4*As_min/pi ) ) /2 ) * 2;   % arrotonda ad un diametro reale
+        arm.tr(i_pl, i_tr).diam = [diam_min;14;14;diam_min];
+        % calcolo freccia
+        ris.tr.s_min(i_pl, i_tr) = calcoloFreccia(L.tr(i_tr), geom.tr, arm.tr(i_pl, i_tr), mat, soll.tr, cost, dx);
 
-rotazione = integraleIndefinito(curvatura,x*1e3,0);
-spostamento = integraleIndefinito(rotazione,x*1e3,0);
+    end
+    toc
+end
+%% Stima dell'armatura
+% l'armatura è stimata con metodo semplificato in funzione del massimo
+% momento in campata
+
+
+
+
+
 
 %%
-    
-
-
-figure(1)
-plot(x,Med)
-figure(2)
-hold on
-plot(x,curvatura)
-plot(x,rotazione)
-plot(x,spostamento)
 
